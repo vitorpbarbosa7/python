10a0038712b73800b135dc7bcfb8356cc1c1896f
vpbmac <vitorpbarbosa7@gmail.com>
2025-11-19 08:04:45 -0300

    anotherversion uv

---
+++ 44_uv/anotherversionuv/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from anotherversionuv!")
+
+
+if __name__ == "__main__":
+    main()
--- 44_uv/datascience/main.py
+++ 44_uv/datascience/main.py
@@ -1,3 +1,6 @@
+import sys
+print(sys.executable)
+
 import numpy as np
 import pandas as pd
 
